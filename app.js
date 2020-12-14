83fe580190a65cbf7c9c7965afc08a8653125da6
Manikanth Reddy <mani@Manikanths-MacBook-Pro.local>
2020-12-14 19:43:49 +0000

    adding message

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const app =express();
 var path = require("path");
 
 app.get("/", function (request, response){
-    //show this file when the "/" is requested
     response.sendFile(__dirname+"/views/index.html");
 });
 
 app.get("/enroll", function (request, response){
-    //show this file when the "/" is requested
     response.sendFile(__dirname+"/views/enroll.html");
 });
 
 
 app.get("/list", function (request, response){
-    //show this file when the "/" is requested
     response.sendFile(__dirname+"/views/list.html");
 });
+
+app.get("/message", function (request, response){
+    response.sendFile(__dirname+"/views/message.html");
+})
 
 app.listen(8081);
 
